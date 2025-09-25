5ccc43a42a6ddf2e22c5943f24e3a0e8bad9231e
이응수 <sistsay@gmail.com>
2025-09-25 16:43:54 +0900

    app

--- src/main/java/com/multi/basic/ArrayEx1.java
+++ src/main/java/com/multi/basic/ArrayEx1.java
@@ -3,7 +3,7 @@ package com.multi.basic;
 import java.util.Scanner;
 //최댓값,최솟값,평균값,평균보다 높은키의 갯수 ,평균보다 낮은 키의 갯수
 public class ArrayEx1 {
-    public static int maxHeight(int []height) {
+    public static int getMaxHeight(int []height) {
         int max = height[0];
         for(int h:height){
             if(h>max){
@@ -12,17 +12,42 @@ public class ArrayEx1 {
         }
         return max;
     }
-    public static int minHeight(int []height) {
-        return 0;
+    public static int getMinHeight(int []height) {
+        int min = height[0];
+        for(int h:height){
+            if(h<min){
+                min= h;
+            }
+        }
+        return min;
+
     }
-    public static double avgHeight(int []height) {
-        return 0;
+    public static double getAvgHeight(int []height) {
+        double sum = 0;
+        for(int h:height){
+            sum += h;
+        }
+        return sum/height.length;
     }
-    public int avgCount(int []height,int state) {
-        return 0;
+    //평균키보다 높은 키의 개수
+    public static int getCountAboveAbverage(int state,int []height) {
+        int count=0;
+        double avg=getAvgHeight(height);
+        for(int h:height){
+            if(state==1&&h>avg){
+                count++;
+            }
+            else if(state==2&&h<avg){
+                count++;
+            }
+        }
+        return count;
     }
-    public void createArray(int []height) {
-    }
+//    //평균키보다 낮은  키의 개수
+//    public static int getCountLowerAverage(){
+//        return 0;
+//    }
+
     public static void main(String[] args) {
 
     }
