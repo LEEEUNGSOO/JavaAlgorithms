ce7bc417fcf6c9629f535663f08c8e41fed484f3
이응수 <sistsay@gmail.com>
2025-09-30 15:18:07 +0900

    code

--- src/main/java/com/multi/basic/queue/PriorityQueueExample.java
+++ src/main/java/com/multi/basic/queue/PriorityQueueExample.java
@@ -15,7 +15,7 @@ public class PriorityQueueExample {
         pq.add(5);
         System.out.println("PriorityQueue contents:"+pq);
 
-        while(!pq.isEmpty()){
+        while(!pq.isEmpty()){//절렬된 결과를 얻고자 하면... poll함수를 이용해서 꺼낸다
             System.out.println(pq.poll());
         }
         System.out.println("PriorityQueue contents:"+pq);
